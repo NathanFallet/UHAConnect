95d2ba08a63ab6accb422ca23366ecfe1ff82f3a
NathanFallet <contact@nathanfallet.me>
2023-06-06 11:11:41 +0200

    API authentication

--- backend/src/main/java/me/nathanfallet/uhaconnect/Application.kt
+++ backend/src/main/java/me/nathanfallet/uhaconnect/Application.kt
@@ -2,7 +2,9 @@ package me.nathanfallet.uhaconnect
 
 import io.ktor.server.application.Application
 import me.nathanfallet.uhaconnect.database.Database
+import me.nathanfallet.uhaconnect.plugins.configureEmails
 import me.nathanfallet.uhaconnect.plugins.configureRouting
+import me.nathanfallet.uhaconnect.plugins.configureSecurity
 import me.nathanfallet.uhaconnect.plugins.configureSerialization
 
 fun main(args: Array<String>): Unit =
@@ -12,5 +14,7 @@ fun Application.module() {
     Database.init(this)
 
     configureSerialization()
+    configureSecurity()
     configureRouting()
+    configureEmails()
 }
---
+++ backend/src/main/java/me/nathanfallet/uhaconnect/plugins/Emails.kt
@@ -0,0 +1,35 @@
+package me.nathanfallet.uhaconnect.plugins
+
+import io.ktor.server.application.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import org.apache.commons.mail.HtmlEmail
+
+object Emails {
+
+    lateinit var host: String
+    lateinit var username: String
+    lateinit var password: String
+
+    fun sendEmail(destination: String, subject: String, content: String) {
+        CoroutineScope(Job()).launch {
+            val email = HtmlEmail()
+            email.hostName = host
+            email.setSmtpPort(587)
+            email.setAuthentication(username, password)
+            email.setFrom(username)
+            email.addTo(destination)
+            email.subject = subject
+            email.setHtmlMsg(content)
+            email.send()
+        }
+    }
+
+}
+
+fun Application.configureEmails() {
+    Emails.host = environment.config.property("email.host").getString()
+    Emails.username = environment.config.property("email.username").getString()
+    Emails.password = environment.config.property("email.password").getString()
+}
