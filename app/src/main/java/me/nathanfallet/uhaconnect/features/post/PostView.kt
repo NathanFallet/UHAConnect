6c8dd08398002d95705d450315eecd4cca3adda9
jeddadothmane <othmane.jeddad20@gmail.com>
2023-06-06 17:12:07 +0200

    basics of post page view

--- app/src/main/java/me/nathanfallet/uhaconnect/features/post/PostView.kt
+++ app/src/main/java/me/nathanfallet/uhaconnect/features/post/PostView.kt
@@ -1,10 +1,18 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,59 +20,80 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
+import me.nathanfallet.uhaconnect.ui.theme.darkBlue
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar(
-    title: String,
-    onBackClicked: () -> Unit
-) {
-    TopAppBar(
-        title = { Text(text = title, color = Color.White) },
-        navigationIcon = {
-            IconButton(onClick = { onBackClicked() }) {
-                Icon(
-                    Icons.Filled.ArrowBack,
-                    contentDescription = "Back",
-                    tint = Color.White
+fun PostView() {
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "UHAConnect",
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentWidth(Alignment.CenterHorizontally)
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { }) {
+                            Icon(
+                                Icons.Filled.ArrowBack,
+                                contentDescription = "Back",
+                                tint = Color.White
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* Action du profil */ }) {
+                            Icon(
+                                Icons.Filled.Person,
+                                contentDescription = "Profile",
+                                tint = Color.White
+                            )
+                        }
+                    },
+                    colors = TopAppBarDefaults.smallTopAppBarColors(
+                        containerColor = darkBlue,
+                        titleContentColor = Color.White
+                    )
                 )
-            }
-        },
-        actions = {
-            IconButton(onClick = { /* Action du profil */ }) {
-                Icon(
-                    Icons.Filled.Person,
-                    contentDescription = "Profile",
-                    tint = Color.White
+
+                Text(
+                    text = "Titre article",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = Color.Black,
+                    modifier = Modifier.padding(vertical = 8.dp)
                 )
+                Text(
+                    text = "Publié par xxx , le jj.mm.annee",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.Black,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+                Text(
+                    text = "Text de test Text de test Text de test Text de test Text de test Text de test Text de test Text de test Text de testText de testText de testText de testText de testText de testText de testText de testText de testText de testText de testText de testText de testText de testText de testText de test",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .padding(horizontal = 8.dp)
+
+                )
+
+
             }
-        },
-        backgroundColor = Color.Blue
-    )
-}
-
-@Composable
-fun App() {
-    MaterialTheme {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            TopBar(title = "UHAConnect", onBackClicked = { /* Action de retour */ })
-
-            Text(
-                text = "Exemple Titre",
-                style = MaterialTheme.typography.h6,
-                color = Color.Black,
-                modifier = Modifier.padding(vertical = 8.dp)
-            )
-
-            // Reste du contenu de votre application
-        }
-    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewApp() {
-    App()
+    PostView()
 }
