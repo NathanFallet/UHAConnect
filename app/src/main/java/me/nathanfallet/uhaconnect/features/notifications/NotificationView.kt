87e2be800513af4d6b1043857018d02fabb4b6de
Salhi Romaissae <salhiromaissae@MacBook-Pro-de-Salhi.local>
2023-06-07 11:27:01 +0200

    ÂNotification

---
+++ app/src/main/java/me/nathanfallet/uhaconnect/features/notifications/NotificationView.kt
@@ -0,0 +1,114 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.nathanfallet.uhaconnect.ui.theme.darkBlue
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String,
+    onBackClicked: () -> Unit
+) {
+    Surface(
+        color = Color.LightGray
+    ) {
+        
+
+    TopAppBar(
+        title = { Text(text = title, color = Color.White) },
+        navigationIcon = {
+            IconButton(onClick = { onBackClicked() }) {
+                Icon(
+                    Icons.Filled.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* Action du profil */ }) {
+                Icon(
+                    Icons.Filled.Person,
+                    contentDescription = "Profile"
+                )
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = darkBlue,
+            titleContentColor = Color.White
+        )
+    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Notifications",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(vertical = 70.dp)
+                .padding(horizontal = 1.dp)
+
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .background(Color.White, shape = MaterialTheme.shapes.medium)
+
+        ) {
+            Row(
+                modifier = Modifier.padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Anonyme",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.Black
+                )
+                Text(
+                    text = " a aimé votre post ",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Black
+                )
+            }
+        }
+    }
+    }
+
+
+}
+
+
+
+
+@Composable
+fun App() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopBar(title = "UHAConnect", onBackClicked = { /* Action de retour */ })
+
+            // Reste du contenu de votre application
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewApp() {
+    App()
+}
+
