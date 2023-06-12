36813043dac6e833bca2b7aab1cfd144ba8d6b29
Tolosmoc <hectochien@gmail.com>
2023-06-12 10:21:56 +0200

    Fix build

--- app/src/main/java/me/nathanfallet/uhaconnect/features/notifications/NotificationView.kt
+++ app/src/main/java/me/nathanfallet/uhaconnect/features/notifications/NotificationView.kt
@@ -1,26 +1,12 @@
 package me.nathanfallet.uhaconnect.features.notifications
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -28,15 +14,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.datetime.Instant
 import me.nathanfallet.uhaconnect.R
 import me.nathanfallet.uhaconnect.extensions.text
-import me.nathanfallet.uhaconnect.models.Notification
-import me.nathanfallet.uhaconnect.models.RoleStatus
-import me.nathanfallet.uhaconnect.models.TypeStatus
 import me.nathanfallet.uhaconnect.models.User
 import me.nathanfallet.uhaconnect.ui.theme.darkBlue
 
@@ -90,7 +71,10 @@ fun NotificationView(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
-                        text = stringResource(id = notification.type.text(), notification.user_id.toString()),
+                        text = stringResource(
+                            id = notification.type.text(),
+                            notification.user?.username ?: ""
+                        ),
                         style = MaterialTheme.typography.bodyMedium,
                     )
                 }
