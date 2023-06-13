073614675db5e8bbb0172afb7fa7200950b3f9de
NathanFallet <contact@nathanfallet.me>
2023-06-13 13:39:44 +0200

    Fixing padding

--- app/src/main/java/me/nathanfallet/uhaconnect/features/compose/ComposeView.kt
+++ app/src/main/java/me/nathanfallet/uhaconnect/features/compose/ComposeView.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -82,7 +81,8 @@ fun ComposeView(
     if (id != null) navigate("post/$id")
 
     LazyColumn(
-        modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         stickyHeader{
             TopAppBar(
