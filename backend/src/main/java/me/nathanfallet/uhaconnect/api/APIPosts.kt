2df089b0ff029b95d0a40cf9d79237ac79f6ffd4
NathanFallet <contact@nathanfallet.me>
2023-06-13 17:11:56 +0200

    Fixing user in notifications and posts/{id}

--- backend/src/main/java/me/nathanfallet/uhaconnect/api/APIPosts.kt
+++ backend/src/main/java/me/nathanfallet/uhaconnect/api/APIPosts.kt
@@ -4,13 +4,35 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
-import io.ktor.server.routing.*
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.put
+import io.ktor.server.routing.route
 import kotlinx.datetime.Clock
 import me.nathanfallet.uhaconnect.database.Database
-import me.nathanfallet.uhaconnect.models.*
+import me.nathanfallet.uhaconnect.models.Comments
+import me.nathanfallet.uhaconnect.models.CreateCommentPayload
+import me.nathanfallet.uhaconnect.models.CreatePostPayload
+import me.nathanfallet.uhaconnect.models.Favorites
+import me.nathanfallet.uhaconnect.models.Follows
+import me.nathanfallet.uhaconnect.models.Notifications
+import me.nathanfallet.uhaconnect.models.Permission
+import me.nathanfallet.uhaconnect.models.Posts
+import me.nathanfallet.uhaconnect.models.RoleStatus
+import me.nathanfallet.uhaconnect.models.TypeStatus
+import me.nathanfallet.uhaconnect.models.UpdatePostPayload
+import me.nathanfallet.uhaconnect.models.Users
 import me.nathanfallet.uhaconnect.plugins.NotificationData
 import me.nathanfallet.uhaconnect.plugins.NotificationsPlugin
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.JoinType
+import org.jetbrains.exposed.sql.Op
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.update
 
 fun Route.apiPosts() {
     route("/posts") {
@@ -113,6 +135,7 @@ fun Route.apiPosts() {
             }
             val post = Database.dbQuery {
                 Posts
+                    .join(Users, JoinType.INNER)
                     .select { Posts.id eq id }
                     .map(Posts::toPost)
                     .singleOrNull()
