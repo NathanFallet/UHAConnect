0e05a68692770e928b05608c19e282e961fbb399
NathanFallet <contact@nathanfallet.me>
2023-06-14 09:49:35 +0200

    Fixing favorites again

--- backend/src/main/java/me/nathanfallet/uhaconnect/api/APIPosts.kt
+++ backend/src/main/java/me/nathanfallet/uhaconnect/api/APIPosts.kt
@@ -49,11 +49,10 @@ fun Route.apiPosts() {
             val posts = Database.dbQuery {
                 Posts
                     .join(Users, JoinType.INNER, Posts.user_id, Users.id)
-                    .join(Favorites, JoinType.LEFT, Favorites.post_id, Posts.id)
-                    .select {
-                        Posts.validated eq true and
-                                (Favorites.user_id eq null or (Favorites.user_id eq user.id))
+                    .join(Favorites, JoinType.LEFT) {
+                        Favorites.post_id eq Posts.id and (Favorites.user_id eq null or (Favorites.user_id eq user.id))
                     }
+                    .select { Posts.validated eq true }
                     .orderBy(Posts.date, SortOrder.DESC)
                     .limit(limit, offset)
                     .map(Posts::toPost)
@@ -107,12 +106,19 @@ fun Route.apiPosts() {
             call.respond(post)
         }
         get("/requests") {
+            val user = getUser() ?: run {
+                call.response.status(HttpStatusCode.Unauthorized)
+                call.respond(mapOf("error" to "Invalid user"))
+                return@get
+            }
             val limit = call.request.queryParameters["limit"]?.toIntOrNull() ?: 10
             val offset = call.request.queryParameters["offset"]?.toLongOrNull() ?: 0L
             val posts = Database.dbQuery {
                 Posts
                     .join(Users, JoinType.INNER, Posts.user_id, Users.id)
-                    .join(Favorites, JoinType.LEFT, Favorites.post_id, Posts.id)
+                    .join(Favorites, JoinType.LEFT) {
+                        Favorites.post_id eq Posts.id and (Favorites.user_id eq null or (Favorites.user_id eq user.id))
+                    }
                     .select { Posts.validated eq false }
                     .orderBy(Posts.date, SortOrder.DESC)
                     .limit(limit, offset)
@@ -134,11 +140,10 @@ fun Route.apiPosts() {
             val post = Database.dbQuery {
                 Posts
                     .join(Users, JoinType.INNER, Posts.user_id, Users.id)
-                    .join(Favorites, JoinType.LEFT, Favorites.post_id, Posts.id)
-                    .select {
-                        Posts.id eq id and
-                                (Favorites.user_id eq null or (Favorites.user_id eq user.id))
+                    .join(Favorites, JoinType.LEFT) {
+                        Favorites.post_id eq Posts.id and (Favorites.user_id eq null or (Favorites.user_id eq user.id))
                     }
+                    .select { Posts.id eq id }
                     .map(Posts::toPost)
                     .singleOrNull()
             } ?: run {
